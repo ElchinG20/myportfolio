14e355a1e0b2ae315e3195182b38c3ed4343b48f
Elchin <101888719+ElchinG20@users.noreply.github.com>
2025-11-30 00:12:46 +0300

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   global: {
     picture: {
-      dark: '/hero/random-1.avif',
-      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      dark: '/hero/my_profile.avif',
+      light: '/hero/my_profile.avif',
       alt: 'My profile picture'
     },
     meetingLink: 'https://cal.com/',
