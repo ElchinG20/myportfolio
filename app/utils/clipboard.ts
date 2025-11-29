64abc370111e9126beb9c4c10b82c6c119fa6ef7
ElchinG20 <101888719+ElchinG20@users.noreply.github.com>
2025-11-29 20:36:03 +0300

    Update clipboard.ts

--- app/utils/clipboard.ts
+++ app/utils/clipboard.ts
@@ -1,4 +1,4 @@
-export function copyToClipboard(toCopy: string, message: string = 'Copied to clipboard') {
+export function copyToClipboard(toCopy: string, message: string = 'Скопировано') {
   const toast = useToast()
   navigator.clipboard.writeText(toCopy).then(() => {
     toast.add({ title: message, color: 'success', icon: 'i-lucide-check-circle' })
