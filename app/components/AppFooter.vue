bee8d83107cf3af2138c15c99fe51e7965aad073
ElchinG20 <elchingasanov.20@gmail.com>
2025-11-30 20:53:54 +0300

    hh

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -9,10 +9,7 @@ const { footer } = useAppConfig()
   >
     <template #left>
       {{ footer.credits }}
-    </template>
-
-    <template #center class="text-center">
-      <UBadge icon="streamline-flex:information-circle" size="lg" color="info" variant="outline">Все системы в норме</UBadge>
+      <UBadge icon="svg-spinners:gooey-balls-2" size="md" color="info" variant="outline">Все системы в норме</UBadge>
     </template>
 
     <template #right>
