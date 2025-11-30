dc1a373504ba24a641c4340f9a85cc9c405ca821
ElchinG20 <elchingasanov.20@gmail.com>
2025-11-30 21:51:12 +0300

    hh

--- app/utils/links.ts
+++ app/utils/links.ts
@@ -4,11 +4,11 @@ export const navLinks: NavigationMenuItem[] = [{
   label: 'Главная',
   icon: 'i-lucide-home',
   to: '/'
-}, {
+}, /* {
   label: 'Проекты',
   icon: 'i-lucide-folder',
   to: '/projects'
-}, {
+}, */ {
   label: 'Блог',
   icon: 'i-lucide-file-text',
   to: '/blog'
@@ -20,4 +20,8 @@ export const navLinks: NavigationMenuItem[] = [{
   label: 'Обо мне',
   icon: 'i-lucide-user',
   to: '/about'
+}, {
+  label: 'Войти',
+  icon: 'i-lucide-user',
+  to: '#'
 }]
