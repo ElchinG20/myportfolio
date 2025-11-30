2770757bbafbc484a77fe7678247c6f0da06a27f
ElchinG20 <elchingasanov.20@gmail.com>
2025-11-30 23:00:38 +0300

    hh

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,6 +19,30 @@ useSeoMeta({
 </script>
 
 <template>
+  <UAvatarGroup>
+    <UAvatar
+      src="https://github.com/benjamincanac.png"
+      alt="Benjamin Canac"
+      :chip="{ inset: true, color: 'success' }"
+    />
+
+    <UAvatar
+      src="https://github.com/romhml.png"
+      alt="Romain Hamel"
+      :chip="{ inset: true, color: 'warning' }"
+    />
+
+    <UAvatar
+      src="https://github.com/noook.png"
+      alt="Neil Richter"
+      :chip="{ inset: true, color: 'error' }"
+    />
+  </UAvatarGroup>
+</template>
+
+
+
+<template>
   <UPage v-if="page">
     <LandingHero :page />
     <UPageSection
