bb2368ddb4cedcf227e5efbab3d94e224390a8a2
ElchinG20 <elchingasanov.20@gmail.com>
2025-12-04 18:49:15 +0300

    hh

--- app/utils/clipboard.ts
+++ app/utils/clipboard.ts
@@ -1,6 +1,6 @@
 export function copyToClipboard(toCopy: string, message: string = 'Скопировано') {
   const toast = useToast()
   navigator.clipboard.writeText(toCopy).then(() => {
-    toast.add({ title: message, color: 'success', icon: 'i-lucide-check-circle' })
+    toast.add({ title: message, color: 'secondary', icon: 'i-lucide-check-circle' })
   })
 }
