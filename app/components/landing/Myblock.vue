a7d559e22d725804a3a2b79a6509eab9c4d42bbf
ElchinG20 <elchingasanov.20@gmail.com>
2025-12-04 19:54:51 +0300

    page.myblock

--- app/components/landing/Myblock.vue
+++ app/components/landing/Myblock.vue
@@ -13,7 +13,10 @@
     description="Скоро на этой странице появится обновление. Следите за новостями."
     icon="svg-spinners:gooey-balls-2"
     orientation="horizontal"
-  >
+    spotlight 
+    spotlight-color="secondary" 
+  >    <!-- spotlight spotlight-color="secondary"  Цвет и движение мыши -->
+  
     <UAvatar class="rounded-none squircle" src="/hero/ra-100.jpg" alt="Эльчин Гасанов" />
     <style>
       .squircle {
