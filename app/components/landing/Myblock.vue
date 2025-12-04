6a9222969f742c6eb05b67611515874f71932e5f
ElchinG20 <elchingasanov.20@gmail.com>
2025-12-04 20:38:28 +0300

    hh

--- app/components/landing/Myblock.vue
+++ app/components/landing/Myblock.vue
@@ -8,7 +8,7 @@
     spotlight spotlight-color="secondary"
   >
 
-    <UAvatar class="w-full rounded-none squircle" src="/hero/ra-100.jpg" alt="Эльчин Гасанов" />
+    <UAvatar class="rounded-none squircle" src="/hero/ra-100.jpg" alt="Эльчин Гасанов" />
     <style>
       .squircle {
         mask-image: url("data:image/svg+xml,%3csvg width='350' height='350' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M100 0C20 0 0 20 0 100s20 100 100 100 100-20 100-100S180 0 100 0Z'/%3e%3c/svg%3e");
