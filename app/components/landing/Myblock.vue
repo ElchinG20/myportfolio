9434cf2c08b38be5b07da738f4b9d6342d8dc49f
ElchinG20 <elchingasanov.20@gmail.com>
2025-12-04 21:13:08 +0300

    hh

--- app/components/landing/Myblock.vue
+++ app/components/landing/Myblock.vue
@@ -15,35 +15,53 @@
 <script setup lang="ts">
 import type { TimelineItem } from '@nuxt/ui'
 
-const items = ref<TimelineItem[]>([
+const items: TimelineItem[] = [
   {
-    date: 'Mar 15, 2025',
-    title: 'Project Kickoff',
-    description: 'Kicked off the project with team alignment.',
-    icon: 'i-lucide-rocket'
+    date: 'Nov 30, 2025',
+    title: 'Запуск проекта',
+    description:
+      'Идея проекта. Установил контрольные точки и распределил ресурсы.',
+    icon: 'i-lucide-rocket',
+    value: 'kickoff'
   },
   {
-    date: 'Mar 22 2025',
-    title: 'Design Phase',
-    description: 'User research and design workshops.',
-    icon: 'i-lucide-palette'
+    date: 'Dec 02, 2025',
+    title: 'Исследования и дизайн',
+    description:
+      'Созданы макеты дизайна и прототипы для тестирования.',
+    icon: 'i-lucide-palette',
+    value: 'design'
   },
   {
-    date: 'Mar 29 2025',
-    title: 'Development Sprint',
-    description: 'Frontend and backend development.',
-    icon: 'i-lucide-code'
+    date: 'Dec 10, 2025',
+    title: 'Спринт разработки',
+    description:
+      'Разработка интерфейса и серверной части. Реализованы основные функции и интеграция с API.',
+    icon: 'i-lucide-code',
+    value: 'development'
   },
   {
-    date: 'Apr 5 2025',
-    title: 'Testing & Deployment',
-    description: 'QA testing and performance optimization.',
-    icon: 'i-lucide-check-circle'
+    date: 'Dec 30, 2025',
+    title: 'Тесты и внедрение',
+    description: 'Тесты качества и оптимизация производительности. Проект в пилоте',
+    icon: 'i-lucide-check-circle',
+    value: 'deployment'
   }
-])
+]
+
+const active = ref(0)
+
+// Note: This is for demonstration purposes only. Don't do this at home.
+onMounted(() => {
+  setInterval(() => {
+    active.value = (active.value + 1) % items.length
+  }, 2000)
+})
 </script>
 
 <template>
-  <UTimeline orientation="horizontal" :default-value="2" :items="items" class="w-full" />
+  <UPageSection>
+    <UTimeline orientation="horizontal" v-model="active" :items="items" class="w-96" />
+  </UPageSection>
 </template>
 
