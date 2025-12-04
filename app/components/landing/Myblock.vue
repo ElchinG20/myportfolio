3bfc96fe845c89c34fc085065eca08f1e9653674
ElchinG20 <elchingasanov.20@gmail.com>
2025-12-04 20:11:46 +0300

    hh

--- app/components/landing/Myblock.vue
+++ app/components/landing/Myblock.vue
@@ -1,20 +1,13 @@
 <template>
-    <UPageSection
-    :title="page.myblock.title"
-    :description="page.myblock.description"
-    :ui="{
-      title: 'text-left text-xl sm:text-xl lg:text-2xl font-medium',
-      description: 'text-left mt-2 text-sm sm:text-md lg:text-sm text-muted'
-    }"
-  >
-  <!-- <UPageCard
+ <UPageSection>
+  <UPageCard
+    title="Скоро обновление"
+    description="Скоро на этой странице появится обновление. Следите за новостями."
     icon="svg-spinners:gooey-balls-2"
     orientation="horizontal"
-    spotlight 
-    spotlight-color="secondary" 
-  >    
-  
-    <UAvatar class="w-full rounded-none squircle" src="/hero/ra-100.jpg" alt="Эльчин Гасанов" />
+    spotlight spotlight-color="secondary"
+  >
+    <UAvatar class="rounded-none squircle" src="/hero/ra-100.jpg" alt="Эльчин Гасанов" />
     <style>
       .squircle {
         mask-image: url("data:image/svg+xml,%3csvg width='200' height='200' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M100 0C20 0 0 20 0 100s20 100 100 100 100-20 100-100S180 0 100 0Z'/%3e%3c/svg%3e");
@@ -23,6 +16,6 @@
         mask-repeat: no-repeat;
       }
     </style>
-  </UPageCard> -->
+  </UPageCard>
  </UPageSection>
 </template>
