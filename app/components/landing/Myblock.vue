de76132103206f5a8149d4a5b27f8880a147e1c9
ElchinG20 <elchingasanov.20@gmail.com>
2025-12-04 19:43:11 +0300

    rounded-none squircle /hero/ra-100.jpg

--- app/components/landing/Myblock.vue
+++ app/components/landing/Myblock.vue
@@ -6,7 +6,15 @@
     icon="svg-spinners:gooey-balls-2"
     orientation="horizontal"
   >
-    <img src="/hero/ra-100.jpg" alt="Tailwind CSS" class="w-full" />
+    <UAvatar class="rounded-none squircle" src="/hero/ra-100.jpg" alt="Эльчин Гасанов" />
+    <style>
+      .squircle {
+        mask-image: url("data:image/svg+xml,%3csvg width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M100 0C20 0 0 20 0 100s20 100 100 100 100-20 100-100S180 0 100 0Z'/%3e%3c/svg%3e");
+        mask-size: contain;
+        mask-position: center;
+        mask-repeat: no-repeat;
+      }
+    </style>
   </UPageCard>
  </UPageSection>
 </template>
