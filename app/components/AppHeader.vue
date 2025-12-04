007e8395d49894c619fbefff3903c829a8b1d1e2
ElchinG20 <elchingasanov.20@gmail.com>
2025-12-04 17:39:57 +0300

    cursor-pointer

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -12,11 +12,10 @@ defineProps<{
       :items="links"
       variant="link"
       color="neutral"
-      class="bg-muted/80 backdrop-blur-sm rounded-full px-2 sm:px-4 border border-muted/50 shadow-lg shadow-neutral-950/5"
+      class="bg-muted/80 backdrop-blur-sm rounded-full px-2 sm:px-4 border border-muted/50 shadow-lg shadow-neutral-950/5 cursor-pointer"
       :ui="{
-        link: 'px-2 py-1',
+        link: 'px-2 py-1 cursor-pointer',
         linkLeadingIcon: 'hidden',
-        class: 'cursor-pointer'
 
       }"
     >
